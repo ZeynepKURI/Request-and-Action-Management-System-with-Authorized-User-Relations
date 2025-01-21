2ebf4d3fc4cb9373ad1af872572cb156d7b5365d
Zeynep Kurı <113287020+ZeynepKURI@users.noreply.github.com>
2025-01-21 15:02:27 +0300

    güncel

---
+++ Core/Domain/Entities/Actions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Domain.Entities
+{
+	public class Actions
+	{
+		public int Id { get; set; }
+
+		public string ActionDescription { get; set; }
+
+		public int RequestId { get; set; }
+
+		public Request Request { get; set; }
+
+		
+	}
+}
+
---
+++ Infrastructure/Persistence/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Context
+{
+    public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext>options) : base(options)
+		{ }
+
+
+		public DbSet<User> users { get; set; }
+
+		public DbSet<Request> requests { get; set; }
+
+		public DbSet<Actions> actions { get; set; }
+
+	}
+}
+
